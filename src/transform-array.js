eec3d478e557e9da481d4bcbaf8a271660b4205b
angietune <angietune@yandex.ru>
2021-03-29 22:09:14 +0300

    feat: transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,29 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+  if (!Array.isArray(arr)) {
+    throw Error;
+  }
+  let arr1 = [];
+  let tr = true;
+  arr.forEach(function (item, i) {
+     if (item === '--discard-next') { 
+      tr = false;
+    } else if (item === '--discard-prev') { 
+      newArray.pop();
+    } else if (item === '--double-next') {  
+      if (i < arr.length - 1) {
+        arr1.push(arr[i + 1]);
+      }
+    } else if (item === '--double-prev') { 
+      if (i > 0) {
+        arr1.push(arr[i - 1]);
+      }
+    } else if (tr) {
+      arr1.push(item);
+    } else {
+      tr = true;
+    }
+  });
+  return arr1;
 };
