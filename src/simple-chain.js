6588041509f9f7495da042e5b70170e9aae6a485
angietune <angietune@yandex.ru>
2021-03-29 18:54:18 +0300

    feat: simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,24 +1,38 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+  chain: [],
   getLength() {
-    throw new CustomError('Not implemented');
+    return this.chain.length;
     // remove line with error and write your code here
   },
   addLink(value) {
-    throw new CustomError('Not implemented');
+    let link = '( )';
+    if (value !== undefined) {
+      link = '( ' + value + ' )';
+    }
+    this.chain.push(link);
+    return this;
     // remove line with error and write your code here
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
+    if (position < 1 || position > this.getLength() || typeof(position) !== 'number') {
+      this.chain = [];
+      throw Error;
+    }
+    this.chain.splice(position - 1, 1);
+    return this;
     // remove line with error and write your code here
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
+    this.chain.reverse();
+    return this;
     // remove line with error and write your code here
   },
   finishChain() {
-    throw new CustomError('Not implemented');
+    let result = this.chain.join('~~');
+    this.chain = [];
+    return result;
     // remove line with error and write your code here
   }
 };
