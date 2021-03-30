aeb424c4e04a0d578305739e46ffe806a78c5070
angietune <angietune@yandex.ru>
2021-03-30 15:45:44 +0300

    fix: transform array bugs

--- src/transform-array.js
+++ src/transform-array.js
@@ -6,24 +6,31 @@ module.exports = function transform(arr) {
   }
   let arr1 = [];
   let tr = true;
-  arr.forEach(function (item, i) {
-     if (item === '--discard-next') { 
+  let next = 0;
+  arr.forEach(function (item, i, arr) {
+      if (item === '--discard-next') { 
       tr = false;
+      next = i + 1;
     } else if (item === '--discard-prev') { 
-      arr1.pop();
+      if (next == 0) arr1.pop();
     } else if (item === '--double-next') {  
-      if (i < arr.length - 1) {
-        arr1.push(arr[i + 1]);
-      }
+        if (i < arr.length - 1) {
+          arr1.push(arr[i + 1]);
+          //next = i + 1;
+        }
     } else if (item === '--double-prev') { 
-      if (i > 0) {
-        arr1.push(arr[i - 1]);
-      }
+        if (i > 0) {
+          if (next == 0) arr1.push(arr[i - 1]);
+        }
     } else if (tr) {
       arr1.push(item);
+      if (i > next) next = 0;
     } else {
       tr = true;
     }
   });
+  //console.log(arr[0]);
+  //console.log(arr1[0]);
   return arr1;
+  
 };
